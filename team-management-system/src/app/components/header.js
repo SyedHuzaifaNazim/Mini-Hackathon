b7adfb6a2f1868a416818b1270683fcbf3e93fab
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-10-20 23:18:53 +0500

    not done yet..

--- team-management-system/src/app/components/header.js
+++ team-management-system/src/app/components/header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { auth } from '../utils/firebase';
+import { auth } from '../firebase/firebaseConfig';
 
 const Header = () => {
   const router = useRouter();

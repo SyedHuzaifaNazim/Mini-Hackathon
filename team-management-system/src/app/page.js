22deb050bc4a1238c56abe663a16bf9dd6a0ef6c
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-10-21 11:18:02 +0500

    not done yet..

--- team-management-system/src/app/page.js
+++ team-management-system/src/app/page.js
@@ -1,15 +1,19 @@
+'use client'
 import Image from "next/image";
 // import SignIn from "./pages/signin/page";
 // import SignUp from "./pages/signup/page";
 // import {email,password} from "firebase/auth"
 import LandingPage from "./components/header";
-import Dashboard from "./pages/dashboard";
+// import Login from "./pages/login"
+import Login from "./pages/login";
+import Dashboard from "./dashboard/page";
 
 
 export default function Home() {
   return (
     <>
-    <LandingPage/>
+    {/* <LandingPage/> */}
+    <Login/>
     <Dashboard/>
     </>
   );

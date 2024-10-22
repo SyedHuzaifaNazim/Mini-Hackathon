b56972e1989160e07b1a20d7dab42cdf90b09a37
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-10-23 01:06:22 +0500

    modified and also renamed

---
+++ team-management-system/src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+import LandingPage from "./components/header";
+import Login from "./pages/login";
+import Dashboard from "./dashboard/page";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+    <>
+    <LandingPage/>
+
+    </>
+  );
+}

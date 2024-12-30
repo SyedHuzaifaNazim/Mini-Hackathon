fba24137afe7ec95652f6405c5321c4c3e510b84
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-12-30 21:35:35 +0500

    some new additions ..

---
+++ team-management-system/src/app/pages/contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <>
+    <div>
+      <h1 className='mb-2'> Contact Us </h1>
+      <p> Phone: 123-456-7890 </p>
+      <p> Email: [info@company.com](mailto:info@company.com) </p>
+      <p> Address: 123 Main St, Anytown, USA </p>
+      
+    </div>
+    </>
+  )
+}
+
+export default contact
